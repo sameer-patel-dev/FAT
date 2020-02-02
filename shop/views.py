b21bae0ed9159bcf72059a98e0824c6875e949a0
Sameer <sameer.patel@somaiya.edu>
2020-02-02 10:29:39 +0530

    new commit

--- shop/views.py
+++ shop/views.py
@@ -145,15 +145,21 @@ def checkout(request):
     return render(request, 'shop/checkout.html')
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 @login_required
 def profile(request):
     return render(request,'user/profile.html')
 =======
+=======
+>>>>>>> 1771f5f3e62492e59b91617eb4157261a23d9882
 def dashboard(request):
     return render(request,"shop/dashboard.html")
 
 def profit_crops(request):
     return render(request,"shop/profit_crops.html")
+<<<<<<< HEAD
+>>>>>>> 1771f5f3e62492e59b91617eb4157261a23d9882
+=======
 >>>>>>> 1771f5f3e62492e59b91617eb4157261a23d9882
 
 @csrf_exempt
